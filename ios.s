91111e0fced7e1edbb7dd6afe825ba683295dbe4
Berathan Yedibela <73439036+Ber4tBey@users.noreply.github.com>
2022-09-16 22:48:48 +0300

    Create ios.s

---
+++ ios.s
@@ -0,0 +1,24 @@
+BERAT+="\n✨Python İnstaller✨"
+MESAJ="\n📱Python Installer Android Setup📱"
+MESAJ+="\n "
+MESAJ+="\n LavanderProjects / Berat Bey"
+MESAJ+="Never Give Up"
+MESAJ+="\n "
+MESAJ+="\n"
+YARDIM="\n"
+YARDIM+="\n "
+BOSLUK="\n "
+echo y | apk update
+clear
+echo -e $BERAT
+echo -e $MESAJ
+echo "Python Yükleniyor"
+echo y | apk add python3
+clear
+echo -e $BERAT
+echo -e $MESAJ
+echo "Git Yükleniyor"
+echo y | apk add git
+clear
+echo "PYTHON & GİT İNDİRMESİ TAMAMLANDI \n\n Berathan Yedibela"
+
