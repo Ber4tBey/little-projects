a094dd8a7edabece7f0320d7fc2818b91d6b11a8
Berathan Yedibela <73439036+Ber4tBey@users.noreply.github.com>
2022-09-16 22:35:00 +0300

    Update python.ps1

--- python.ps1
+++ python.ps1
@@ -1,12 +1,13 @@
 Write-Output("Berathan Yedibela")
-Write-Output("⌛ Scoop Yükleniyor...")
+Write-Output("Scoop Yukleniyor...")
+scoop update
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-Write-Output("⌛ Github yükleniyor...")
-scoop update
+Write-Output(" Github yükleniyor...")
+
 scoop install git
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 Write-Output(" Berathan Yedibela Windows Auto Python Downloader ")
-Write-Output("⌛ İnstalling Python & Git ")
+Write-Output(" İnstalling Python & Git ")
 scoop install git python@3.8
 Write-Output("İndirme tamamlandı!!")
